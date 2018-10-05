e4b608e8141711d1dc02b59f6a93350149a36882
Andrea Perez Huizar <39924790+maph25@users.noreply.github.com>
2018-10-05 15:55:26 -0500

    Main without initilizations

--- main.c
+++ main.c
@@ -12,10 +12,94 @@
 #include "Delay.h"
 #include "Key.h"
 
+uint8_t state;
+uint8_t digit;
+uint8_t slct;
+uint8_t password[4];
+uint8_t DataAvailable;
+
+#define INITIAL_STATE 0u
+#define DGT_ONE 1u
+#define DGT_TWO 2u
+#define DGT_THREE 3u
+#define DGT_FOUR 4u
+#define DGT_PASS 5u
+
 int main(void) {
 
     while(1) {
+    	for(state = 0; state > DGT_FOUR; state ++){
+    		if(state > DGT_FOUR){
+    				state = INITIAL_STATE;
+    			}
+    			else{
+    				DataAvailable = GPIO_get_flag();
+    			state = DGT_ONE;
+    			switch (state) {
+    				case DGT_ONE:
+    					if(DataAvailable == TRUE)
+    					{
+    						digit = KEY_get_key();
+    						password[0] = KEY_decode(digit);
+    						state = DGT_TWO;
+    					}
+    					break;
+    				case DGT_TWO:
+    					if(DataAvailable == TRUE)
+    					{
+    						digit = KEY_get_key();
+    						password[1] = KEY_decode(digit);
+    						state = DGT_THREE;
+    					}
+    					break;
+    				case DGT_THREE:
+    					if(DataAvailable == TRUE)
+    					{
+    						digit = KEY_get_key();
+    						password[2] = KEY_decode(digit);
+    						state = DGT_FOUR;
+    					}
+    					break;
+    				case DGT_FOUR:
+    					if(DataAvailable == TRUE)
+    					{
+    						digit = KEY_get_key();
+    						password[3] = KEY_decode(digit);
+    						state = DGT_PASS;
+    					}
+    					break;
+    				case DGT_PASS:
+    					if (password[0] == KEY_ONE && password[1] == KEY_TWO && password[2] == KEY_THREE && password[3] == KEY_FOUR){
+    						GPIO_toogle_pin(GPIO_A, bit_2); /**Turn on correct password led*/
+    						digit = KEY_get_key();
+    						slct = KEY_decode(digit);
+    						switch(slct){
+    							case KEY_A:
+    								GPIO_toogle_pin(GPIO_A, bit_2); /**Turn on correct password led*/
+    								break;
+    							case KEY_B:
+    								GPIO_toogle_pin(GPIO_A, bit_2); /**Turn on correct password led*/
+    								break;
+    							default:
+    								break;
+    							/*ERROR*/
+    						}
+    					}
+    					else if(password[0] == KEY_FOUR && password[1] == KEY_FIVE && password[2] == KEY_SIX && password[3] == KEY_SEVEN){
+    							GPIO_toogle_pin(GPIO_A, bit_2); /**Turn on correct password led*/
+    					}
+    					else if(password[0] == KEY_SEVEN && password[1] == KEY_EIGHT && password[2] == KEY_NINE && password[3] == KEY_ZERO){
+    							GPIO_toogle_pin(GPIO_A, bit_2); /**Turn on correct password led*/
+    					}
+    				/*ERROR*/
+    				default:
+    					break;
 
-    }
+    			}
+    		}/*Else*/
+
+    	}/*For*/
+
+    }/*While*/
     return 0 ;
 }
