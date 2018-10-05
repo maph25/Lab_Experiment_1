20eb02388fc826fc06eb7fcd14ec1e4c7654749a
Andrea Perez Huizar <39924790+maph25@users.noreply.github.com>
2018-10-05 13:16:16 -0500

    Delay

---
+++ Delay.c
@@ -0,0 +1,16 @@
+/*
+ * Delay.c
+ *
+ *  Created on: Oct 5, 2018
+ *      Author: Andy
+ */
+#include  "Delay.h"
+
+void delay(uint16_t delay)
+{
+	volatile uint16_t counter;
+
+	for(counter = delay; counter > 0; counter--)
+	{
+	}
+}
---
+++ Delay.h
@@ -0,0 +1,17 @@
+/*
+ * Delay.h
+ *
+ *  Created on: Oct 5, 2018
+ *      Author: Andy
+ */
+
+#ifndef DELAY_H_
+#define DELAY_H_
+
+#include "stdint.h"
+
+#define DELAY 65000
+
+void delay(uint16_t delay);
+
+#endif /* DELAY_H_ */
